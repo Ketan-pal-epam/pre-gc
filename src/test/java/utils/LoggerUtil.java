5cbfbdda85453e3a38dc52391cf6b48a4c2c3d39
Ketan Pal <ketan_pal@epam.com>
2024-10-29 22:46:53 +0530

    implemented loggers

--- src/test/java/utils/LoggerUtil.java
+++ src/test/java/utils/LoggerUtil.java
@@ -1,15 +1,40 @@
 package Utils;
 
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
-public class LoggerUtil {
-    private static Logger log = LogManager.getLogManager().getLogger("logger");
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-    public static void info(String msg){
-        log.info(msg);
+
+
+
+public class LoggerUtil implements ITestListener {
+    private static final Logger LOG = LogManager.getLogger(LoggerUtil.class);
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        LOG.info("Starting test: {}", result.getName());
     }
-    public static void error(String msg){
-        log.warning(msg);
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        LOG.info("Test passed: {}", result.getName());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        LOG.error("Test failed: {}", result.getName());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        LOG.warn("Test skipped: {}", result.getName());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        LOG.info("Finished all tests in context: {}", context.getName());
     }
 }
